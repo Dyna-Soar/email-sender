6aac7e3f722a8e837f9fc441b0972a5cbf62f696
Bodin <bodintiago@gmail.com>
2022-05-23 21:21:14 +0200

    send one mail to receiver

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-import smtplib
 import os
 
-MY_ADDRESS = os.environ.get("MY_ADDRESS")
-PASSWORD = os.environ.get("PASSWORD")
+import smtplib
 
-s = smtplib.SMTP(host='localhost', port=80)
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MY_ADDRESS = os.getenv("MY_ADDRESS")
+PASSWORD = os.getenv("PASSWORD")
+print(MY_ADDRESS)
+
+
+RECEIVER = os.getenv("RECEIVER")
+
+
+s = smtplib.SMTP(host="smtp.gmail.com", port=587)
 s.starttls()
 s.login(MY_ADDRESS, PASSWORD)
+
+
+msg = MIMEMultipart()
+
+msg['From'] = MY_ADDRESS
+msg['To'] = RECEIVER
+msg['Subject'] = "SMTP message"
+
+message = "hello, this is a message"
+
+msg.attach(MIMEText(message, 'plain'))
+
+s.send_message(msg)
